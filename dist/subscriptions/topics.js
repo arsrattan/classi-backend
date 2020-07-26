c046fbe792e9b09062006d05ed6226741d0a9a30
malikhadjri <mjhadjri@gmail.com>
2020-07-26 16:47:26 -0400

    remove dist and serveless dirs

--- dist/subscriptions/topics.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Topic = void 0;
-var Topic;
-(function (Topic) {
-    Topic["NewComment"] = "NEW_COMMENT";
-    Topic["NewClass"] = "NEW_CLASS";
-})(Topic = exports.Topic || (exports.Topic = {}));
-//# sourceMappingURL=topics.js.map

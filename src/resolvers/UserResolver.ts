cae5bd9f242952fcdbad4ff64fbe6549df43ff5c
Anmol Rattan <anmolrattan@Anmols-MacBook-Pro.local>
2020-08-08 10:39:33 -0400

    uncommented

--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -40,11 +40,11 @@ export class UserResolver {
     return await this.userController.getUserFollowing(userId);
   }
 
-//   // @UseMiddleware(isCorrectUser)
-//   @Query((returns) => [Notification], { nullable: true })
-//   async getUserNotifications(@Arg("userId") userId: string) {
-//     return await this.userController.getUserNotifications(userId);
-//   }
+  //   // @UseMiddleware(isCorrectUser)
+  //   @Query((returns) => [Notification], { nullable: true })
+  //   async getUserNotifications(@Arg("userId") userId: string) {
+  //     return await this.userController.getUserNotifications(userId);
+  //   }
 
   // @UseMiddleware(isAuth)
   @Query((returns) => Number, { nullable: true })
@@ -52,13 +52,13 @@ export class UserResolver {
     return await this.userController.getNumFollowers(userId);
   }
 
-//   @Mutation((returns) => AuthData)
-//   async login(
-//     @Arg("username") username: string,
-//     @Arg("password") password: string
-//   ) {
-//     return await this.userController.login(username, password);
-//   }
+  @Mutation((returns) => AuthData)
+  async login(
+    @Arg("username") username: string,
+    @Arg("password") password: string
+  ) {
+    return await this.userController.login(username, password);
+  }
 
   // @UseMiddleware(isCorrectUser)
   @Mutation((returns) => Boolean)
@@ -95,34 +95,34 @@ export class UserResolver {
     // return await this.userController.registerUser(data);
   }
 
-//   // @UseMiddleware(isCorrectUserFromConfirmation)
-//   @Mutation((returns) => Boolean)
-//   async confirmUser(@Arg("token") token: string) {
-//     return await this.userController.confirmUser(token);
-//   }
+  //   // @UseMiddleware(isCorrectUserFromConfirmation)
+  //   @Mutation((returns) => Boolean)
+  //   async confirmUser(@Arg("token") token: string) {
+  //     return await this.userController.confirmUser(token);
+  //   }
 
-//   // @UseMiddleware(isCorrectUserFromConfirmation)
-//   @Mutation((returns) => Boolean)
-//   async changePassword(
-//     @Arg("token") token: string,
-//     @Arg("password") password: string
-//   ) {
-//     return await this.userController.changePassword(token, password);
-//   }
+  //   // @UseMiddleware(isCorrectUserFromConfirmation)
+  //   @Mutation((returns) => Boolean)
+  //   async changePassword(
+  //     @Arg("token") token: string,
+  //     @Arg("password") password: string
+  //   ) {
+  //     return await this.userController.changePassword(token, password);
+  //   }
 
-//   @Mutation((returns) => Boolean)
-//   async forgotPassword(@Arg("email") email: string) {
-//     return await this.userController.forgotPassword(email);
-//   }
+  //   @Mutation((returns) => Boolean)
+  //   async forgotPassword(@Arg("email") email: string) {
+  //     return await this.userController.forgotPassword(email);
+  //   }
 
-//   // @UseMiddleware(isCorrectUser)
-//   @Mutation((returns) => Boolean)
-//   async updateUser(
-//     @Arg("data", { nullable: true }) data: UserInput,
-//     @Arg("userId") userId: string
-//   ) {
-//     return await this.userController.updateUser(userId, data);
-//   }
+  //   // @UseMiddleware(isCorrectUser)
+  //   @Mutation((returns) => Boolean)
+  //   async updateUser(
+  //     @Arg("data", { nullable: true }) data: UserInput,
+  //     @Arg("userId") userId: string
+  //   ) {
+  //     return await this.userController.updateUser(userId, data);
+  //   }
 
   // @UseMiddleware(isCorrectUser)
   @Mutation((returns) => Boolean)
